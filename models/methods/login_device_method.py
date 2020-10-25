696b8042b66c4c636372089092fd6b23a369b382
K4CZP3R <kacperserewis@outlook.com>
2020-10-26 00:07:56 +0100

    Add missing property to login device method

--- models/methods/login_device_method.py
+++ models/methods/login_device_method.py
@@ -4,4 +4,5 @@ from typing import Any
 class LoginDeviceMethod(method.Method):
     def __init__(self, params: Any):
         super().__init__("login_device", params)
+        self.requestTimeMils = 0
 
